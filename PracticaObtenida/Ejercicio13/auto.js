dcc847b668814262ee5619513297c80285276198
Christian Heck <christianpaulheck@hotmail.com>
2021-10-30 00:10:17 -0300

    Ejer 13

---
+++ PracticaObtenida/Ejercicio13/auto.js
@@ -0,0 +1,15 @@
+class Car {
+  constructor( make, model, year, color ) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+  }
+
+  setColor( color ) {
+    this.color = color;
+  }
+}
+
+//let subaru = new Car( 'Subaru', 'Outback', 2005, 'Grey' );
+//Auto.setColor( 'Red' );
